500e4e8a005fef12a4bc57c796542cd62048f72b
Tawunchai Burakhon <161191262+Tawunchai@users.noreply.github.com>
2025-04-11 02:28:03 +0700

    add

---
+++ frontend/src/page/Dashboard/body/Chart_Temp_Humi.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const Chart1 = () => {
+    const [chart1Data, setChart1Data] = useState([
+        { name: 'DAY 1', Temperature: 4000, Humidity: 2400, amt: 2400 },
+        { name: 'DAY 2', Temperature: 3000, Humidity: 1398, amt: 2210 },
+        { name: 'DAY 3', Temperature: 2000, Humidity: 9800, amt: 2290 },
+        { name: 'DAY 4', Temperature: 2780, Humidity: 3908, amt: 2000 },
+        { name: 'DAY 5', Temperature: 1890, Humidity: 4800, amt: 2181 },
+        { name: 'DAY 6', Temperature: 2390, Humidity: 3800, amt: 2500 },
+        { name: 'DAY 7', Temperature: 3490, Humidity: 4300, amt: 2100 },
+    ]);
+
+    return (
+        <div className='card my-4 shadow-md sm:rounded-lg bg-white'>
+            <div className='flex items-center justify-between px-5 py-5 pb-0'>
+                <h2 className="text-[18px] font-[700]">Temperature and Humidity Data Chart</h2>
+            </div>
+
+            <div className='flex items-center gap-5 px-5 py-5 pt-2'>
+                <span className='flex items-center gap-1 text-[15px]'><span className='block w-[8px] h-[8px] rounded-full bg-red-400'></span>Tempreture</span>
+                <span className='flex items-center gap-1 text-[15px]'><span className='block w-[8px] h-[8px] rounded-full bg-blue-400'></span>Humidity</span>
+            </div>
+
+            <div className="w-full h-[500px] px-5">
+                <ResponsiveContainer width="100%" height="100%">
+                    <LineChart
+                        data={chart1Data}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" tick={{fontSize:12}}/>
+                        <YAxis tick={{fontSize:12}}/>
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="Temperature" stroke="#8884d8" strokeWidth={3} activeDot={{ r: 8 }} />
+                        <Line type="monotone" dataKey="Humidity" stroke="#82ca9d" strokeWidth={3}/>
+                    </LineChart>
+                </ResponsiveContainer>
+            </div>
+        </div>
+    );
+};
+
+export default Chart1;
--- frontend/src/page/Dashboard/body/dashboard.tsx
+++ frontend/src/page/Dashboard/body/dashboard.tsx
@@ -2,24 +2,151 @@ import DashboardBoxs from "./dashboardboxs"
 import picture1 from "../../../assets/ESP32.png"
 import { FaPlus } from "react-icons/fa6";
 import { Button } from "@mui/material";
+import Chart1 from "./Chart_Temp_Humi";
+import Chart2 from "./Chart_Formaldehyde";
 
 const dashboard = () => {
+
   return (
     <>
-    <div className="w-full py-2 px-5 border bg-white border-[rgba(0,0,0,0.1)] flex items-center gap-8 mb-5 justify-between rounded-md">
+      <div className="w-full py-2 px-5 border bg-white border-[rgba(0,0,0,0.1)] flex items-center gap-8 mb-5 justify-between rounded-md">
         <div className="info">
-            <h1 className="text-[35px] font-bold leading-10 mb-3">Good Morning,<br />
+          <h1 className="text-[35px] font-bold leading-10 mb-3">Good Morning,<br />
             Cameroni</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam culpa, ab velit eos totam voluptas!</p>
-              <br />
-            <Button className="btn-blue !capitalize"><FaPlus className="mr-2"/> Add Hardware IP</Button>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam culpa, ab velit eos totam voluptas!</p>
+          <br />
+          <Button className="btn-blue !capitalize"><FaPlus className="mr-2" /> Add Hardware IP</Button>
         </div>
 
         <img src={picture1} className="w-[230px]" />
 
 
-    </div>
-    <DashboardBoxs/>
+      </div>
+      <DashboardBoxs />
+
+      <div className="card my-5 shadow-md sm:rounded-lg bg-white border border-[rgba(0,0,0,0.1)]">
+        <div className="flex items-center justify-between px-5 py-5">
+          <h2 className="text-[18px] font-[700]">Recent Data Hardware</h2>
+        </div>
+        <div className="relative overflow-x-auto">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-20 dark:bg-gray-50 dark:text-gray-700">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Data No
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Formaldehyde
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Temperature
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Humidity
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="odd:bg-white odd:dark:bg-gray-10 even:bg-gray-50 even:dark:bg-gray-100 border-b dark:border-gray-100 border-gray-200">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-dark">
+                  Apple MacBook Pro 17
+                </th>
+                <td className="px-6 py-4">
+                  Silver
+                </td>
+                <td className="px-6 py-4">
+                  Laptop
+                </td>
+                <td className="px-6 py-4">
+                  $2999
+                </td>
+                <td className="px-6 py-4">
+                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+              </tr>
+              <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-100 border-b dark:border-gray-100 border-gray-200">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-dark">
+                  Microsoft Surface Pro
+                </th>
+                <td className="px-6 py-4">
+                  White
+                </td>
+                <td className="px-6 py-4">
+                  Laptop PC
+                </td>
+                <td className="px-6 py-4">
+                  $1999
+                </td>
+                <td className="px-6 py-4">
+                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+              </tr>
+              <tr className="odd:bg-white odd:white:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-100 border-gray-200">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-dark">
+                  Magic Mouse 2
+                </th>
+                <td className="px-6 py-4">
+                  Black
+                </td>
+                <td className="px-6 py-4">
+                  Accessories
+                </td>
+                <td className="px-6 py-4">
+                  $99
+                </td>
+                <td className="px-6 py-4">
+                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+              </tr>
+              <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-100 border-b dark:border-gray-100 border-gray-200">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-dark">
+                  Google Pixel Phone
+                </th>
+                <td className="px-6 py-4">
+                  Gray
+                </td>
+                <td className="px-6 py-4">
+                  Phone
+                </td>
+                <td className="px-6 py-4">
+                  $799
+                </td>
+                <td className="px-6 py-4">
+                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-dark">
+                  Apple Watch 5
+                </th>
+                <td className="px-6 py-4">
+                  Red
+                </td>
+                <td className="px-6 py-4">
+                  Wearables
+                </td>
+                <td className="px-6 py-4">
+                  $999
+                </td>
+                <td className="px-6 py-4">
+                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="flex gap-4 px-0 py-0">
+        <div className="flex-1">
+          <Chart1 />
+        </div>
+        <div className="flex-1">
+          <Chart2 />
+        </div>
+      </div>
     </>
   )
 }
